1d7dec92a982301724226ba74724a91129834269
Mario Michel <1108mario@gmail.com>
2019-07-06 18:14:19 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
 # Phase 1 Standart Hosts to check
-echo google.at >> hosts.txt
-echo 1.1.1.1 >> hosts.txt
+echo 'google.at' >> hosts.txt
+echo '1.1.1.1' >> hosts.txt
 echo "Standart Hosts to check has been set"
+echo Enter user for systemd
+read USER
+echo "[i] : Searching for installation path ... This can take a few moments"
+INSTALLATIONPATH=`find / -name "*margios" 2>/dev/null`
+echo "Installation path : $installpath"
+echo "[i] : Found installation path"
 
 # Phase 2 Seting up Systemd Service
 echo "[Unit]" >> /etc/systemd/system/margios.service
-echo "Description=Marios's Nagius" >> /etc/systemd/system/margios.service
+echo "Description=Marios's Nagius @ https://github.com/MrMarioMichel/margios" >> /etc/systemd/system/margios.service
 echo "After=network-online.target" >> /etc/systemd/system/margios.service
 echo "StartLimitIntervalSec=0" >> /etc/systemd/system/margios.service
 echo "" >> /etc/systemd/system/margios.service
 echo "[Service]" >> /etc/systemd/system/margios.service
 echo "Type=simple" >> /etc/systemd/system/margios.service
-echo "User=pi" >> /etc/systemd/system/margios.service
-echo "ExecStart=/home/pi/scripts/margios.sh" >> /etc/systemd/system/margios.service
+echo "User=$USER" >> /etc/systemd/system/margios.service
+echo "ExecStart=$INSTALLATIONPATH" >> /etc/systemd/system/margios.service
 echo "" >> /etc/systemd/system/margios.service
 echo "[Install]" >> /etc/systemd/system/margios.service
 echo "WantedBy=multi-user.target" >> /etc/systemd/system/margios.service
@@ -27,7 +33,7 @@ systemctl status margios.service
 echo "Autostartup was enabled"
 
 # Phase 4 Install telegram-send 
-sudo apt-get install python3-pip
-sudo pip3 install telegram-send
+apt-get install python3-pip
+pip3 install telegram-send
 telegram-send --configure
 echo "Telegram send has been configured"
